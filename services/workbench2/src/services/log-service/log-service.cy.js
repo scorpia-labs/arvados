dabad4aa74fdb05dfdf2bf97bbd55950a7ac7aa4
Scorpia Labs <scorpia.labs.wz@gmail.com>
2026-09-15 22:24:50 -0400

    Update calls to `cy.stub()` for Cypress 15.

--- services/workbench2/src/services/log-service/log-service.cy.js
+++ services/workbench2/src/services/log-service/log-service.cy.js
@@ -124,13 +124,13 @@ describe("LogService", () => {
         const containerRequest = {uuid: 'zzzzz-xvhdp-000000000000000', containerUuid: 'zzzzz-dz642-000000000000000'};
         const fileRecord = {name: `stdout.txt`};
         const fileContents = `Line 1\nLine 2\nLine 3`;
-        cy.stub(apiWebdavClient, 'get', (path, options) => {
-                const matches = /bytes=([0-9]+)-([0-9]+)/.exec(options.headers?.Range || '');
-                if (matches?.length === 3) {
-                    return Promise.resolve({responseText: fileContents.substring(Number(matches[1]), Number(matches[2]) + 1)})
-                }
-                return Promise.reject();
-            })
+        cy.stub(apiWebdavClient, 'get').callsFake((path, options) => {
+            const matches = /bytes=([0-9]+)-([0-9]+)/.exec(options.headers?.Range || '');
+            if (matches?.length === 3) {
+                return Promise.resolve({responseText: fileContents.substring(Number(matches[1]), Number(matches[2]) + 1)})
+            }
+            return Promise.reject();
+        });
 
         // when
         let result = await logService.getLogFileContents(containerRequest, fileRecord, 0, 3);
